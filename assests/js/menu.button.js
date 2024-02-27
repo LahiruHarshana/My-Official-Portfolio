c10c3dd345052560146066904e570041e94af019
LahiruHarshana <lharshana2002@gmail.com>
2024-02-27 19:51:07 +0530

    updated

--- assests/js/menu.button.js
+++ assests/js/menu.button.js
@@ -1,23 +1,30 @@
-
-$(document).ready(function(){
-    $('.mobile_menu_icon').click(function(){
+$(document).ready(function () {
+    $('.mobile_menu_icon').click(function () {
         $('.mobile_nav').toggleClass('active');
     });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const outerDivs = document.querySelectorAll(".sec6Container > div");
 
-    outerDivs.forEach(function(outerDiv) {
-        outerDiv.addEventListener("mouseenter", function() {
+    outerDivs.forEach(function (outerDiv) {
+        outerDiv.addEventListener("mouseenter", function () {
             const innerDiv = outerDiv.querySelector(".invisible");
             innerDiv.style.display = "block";
         });
 
-        outerDiv.addEventListener("mouseleave", function() {
+        outerDiv.addEventListener("mouseleave", function () {
             const innerDiv = outerDiv.querySelector(".invisible");
             innerDiv.style.display = "none";
         });
     });
 });
+
+    var swiper = new Swiper(".mySwiper", {
+    pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+},
+});
+
 
